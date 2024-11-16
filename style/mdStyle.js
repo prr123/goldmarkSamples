e5561c414bf0ead524d92fef0bf1c424c380bfda
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2024-11-16 17:07:24 +0100

    added default style

--- style/mdStyle.js
+++ style/mdStyle.js
@@ -1,1 +1,8 @@
-let mdStyle = {};
+let mdStyle = {
+	h1: {fontSize: '2rem', lineHeight: '2'},
+	h2: {fontSize: '1.5rem'},
+	h3: {fontSize: '1.2rem'},
+	h4: {fontSize: '1rem'},
+	h5: {fontSize: '1rem'},
+	h6: {fontSize: '1rem'},
+};
