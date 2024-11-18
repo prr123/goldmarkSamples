7fc61727895ce8070b455fb9aef82a05b76b4e15
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2024-11-18 17:54:19 +0100

    expanded default style

--- style/mdStyle.js
+++ style/mdStyle.js
@@ -1,8 +1,11 @@
 let mdStyle = {
-	h1: {fontSize: '2rem', lineHeight: '2'},
+	h1: {fontSize: '2rem', margin: '0 1rem',},
 	h2: {fontSize: '1.5rem'},
 	h3: {fontSize: '1.2rem'},
 	h4: {fontSize: '1rem'},
 	h5: {fontSize: '1rem'},
 	h6: {fontSize: '1rem'},
+	p: {margin: '1rem 0'},
+	code: {fontFamily: 'monospace'},
+	block: {margin: '0px 40px'},
 };
